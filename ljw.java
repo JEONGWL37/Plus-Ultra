c332d9f6810a8a4625652eb6f1edb160135c9ffd
navylsyio <navylsyio@naver.com>
2024-03-18 11:56:36 +0900

    스테이지 테스트 중

---
+++ ljw.java
@@ -0,0 +1,36 @@
+// import java.util.*;
+
+
+// public class ljw {
+
+
+//   public static void main(String[] args) {
+
+//     int[] paper1 =new int[10];
+//     int paper2[] =new int[10];
+//     int[] paper3 = {35,37,39,57,53,59,73,75,77};
+//     char[] star = {'a', 'b', 'c', 'd'};
+
+//     for (int i=0; i < paper1.length; i++) {
+//       paper1[i] = i+1;
+//     }
+
+//     for (int i=0; i < paper2.length; i++) {
+//       paper2[i] = (int)(Math.random()*10)+1;
+//     }
+
+//     for(int i=0; i < paper1.length; i++) {
+//       System.out.print(paper1[i]+",");
+//     }
+//     System.out.println();
+
+//     System.out.println(Arrays.toString(paper2));
+//     System.out.println(Arrays.toString(paper3));
+//     System.out.println(Arrays.toString(star));
+//     System.out.println((paper3));
+//     System.out.println((star));
+
+
+//   }
+
+// }
